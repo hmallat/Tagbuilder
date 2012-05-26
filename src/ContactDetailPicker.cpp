871aa41901764f98b86531fe21a93420618a36c3
Hannu Mallat <hmallat@gmail.com>
2012-05-26 08:40:21 +0300

    GPLv3 release

--- src/ContactDetailPicker.cpp
+++ src/ContactDetailPicker.cpp
@@ -1,10 +1,22 @@
 /*
- * This file is part of NFC tag creator
- *
- * Copyright (C) 2011 Hannu Mallat. All rights reserved. 
- * Contact: Hannu Mallat <hmallat@gmail.com>
- *
- */
+
+    NFC Tag Builder for Nokia N9
+    Copyright (C) 2011,2012  Hannu Mallat <hmallat@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #include "ContactDetailPicker.h"
 #include "ContactDetailPickerListCellCreator.h"
